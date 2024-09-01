f0008a032de1404cf5a98db0475bba22c98bba3e
netpl <31539268+netpl@users.noreply.github.com>
2024-09-01 14:05:50 +0900

    V5.2

--- app.js
+++ app.js
@@ -101,7 +101,8 @@ function updateChart(teacherName, voteHistory) {
                 backgroundColor: 'rgba(75, 192, 192, 0.2)',
                 borderColor: 'rgba(75, 192, 192, 1)',
                 borderWidth: 1,
-                fill: false
+                fill: false,
+                tension: 0.4
             }]
         },
         options: {
